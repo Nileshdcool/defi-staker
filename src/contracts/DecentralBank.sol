4073445dcc43d67faef0fe1163eb1822b9b93f9e
nilesh-sukalikar <neil.sukalikar@gmail.com>
2023-08-08 21:03:30 +0530

    staking feature added for decentral bank

--- src/contracts/DecentralBank.sol
+++ src/contracts/DecentralBank.sol
@@ -10,10 +10,33 @@ contract DecentralBank {
     address public owner;
     Teather public teather;
     RWD public rwd;
+    address[] public stakers;
+    mapping(address => uint) public stakingBalance;
+    mapping(address => bool) public hasStaked;
+    mapping(address => bool) public isStaked;
 
     constructor(RWD _rwd, Teather _teather) public {
         rwd = _rwd;
         teather = _teather;
+    }
+     
+
+     // staking function
+    function depositTokens(uint _amount) public {
+        // require staking amount is always greater than zero
+        require(_amount > 0, "amount can't be zero");
+
+        // tranmfer treather tokens to this contract address from staking
+        teather.tranferFrom(msg.sender, address(this), _amount);
+        // update staking balance
+        stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount;
+
+        if(!hasStaked[msg.sender]) {
+            stakers.push(msg.sender);
+        }
+        // update staking balance
+        isStaked[msg.sender] = true;
+        hasStaked[msg.sender] = true;
     }
 
 }
--- test/decentralBank.js
+++ test/decentralBank.js
@@ -1,7 +1,4 @@
 // const { assert } = require('console');
-
-
-
 const Teather = artifacts.require('Teather');
 const RWD = artifacts.require('RWD');
 const DecentralBank = artifacts.require('DecentralBank');
@@ -11,14 +8,14 @@ require('chai')
     .use(require('chai-as-promised'))
     .should()
 
-contract('DecentralBank', ([owner,customer]) => {
+contract('DecentralBank', ([owner, customer]) => {
     let teather, rwd, decentralBank;
 
     function tokens(number) {
         return web3.utils.toWei(number, 'ether');
     }
 
-    before(async()=> {
+    before(async () => {
         // Load cntracts
         teather = await Teather.new();
         rwd = await RWD.new();
@@ -28,7 +25,7 @@ contract('DecentralBank', ([owner,customer]) => {
 
         // treamfer 100 mock teather to customer
 
-        await teather.transfer(customer,tokens('100'), {from:owner});
+        await teather.transfer(customer, tokens('100'), { from: owner });
     })
 
     describe('Mock teather Deployment', async () => {
@@ -40,7 +37,7 @@ contract('DecentralBank', ([owner,customer]) => {
 
     describe('Reward Token Deployment', async () => {
         it('matched name successdully', async () => {
-            
+
             const name = await rwd.name();
             assert.equal(name, 'Reward Token');
         })
@@ -48,14 +45,43 @@ contract('DecentralBank', ([owner,customer]) => {
 
     describe('Decentral Bank Deployment', async () => {
         it('matched name successdully', async () => {
-            
+
             const name = await decentralBank.name();
             assert.equal(name, 'Decentral Bank');
         })
 
-        it('contract has tokens', async() => {
+        it('contract has tokens', async () => {
             let balance = await rwd.balanceOf(decentralBank.address);
             assert.equal(balance, tokens('1000000'));
+        })
+    })
+
+    describe("yield Farming", async () => {
+        it('reward tokens for staking', async () => {
+            let result;
+
+            // checking Investors Balance
+
+            result = await teather.balanceOf(customer);
+            assert.equal(result.toString(), tokens('100'), 'customer mock balance before staking');
+
+            // check staking for custome 100 tokens
+            await teather.approve(decentralBank.address, tokens('100'), {from: customer})
+            await decentralBank.depositTokens(tokens('100'),{from:customer});
+
+            // check updated balance of customer
+
+            result = await teather.balanceOf(customer);
+            assert.equal(result.toString(), tokens('0'), 'customer mock wallet after staking')
+
+            // check updated balance of decentrrak bank 
+
+            result = await teather.balanceOf(decentralBank.address);
+            assert.equal(result.toString(), tokens('100'), 'decentral bank mock wallet after staking')
+
+            // isStaking balance
+            result = await decentralBank.isStaked(customer);
+            assert.equal(result.toString(), 'true', 'customer is staking status after staking')
         })
     })
 });
